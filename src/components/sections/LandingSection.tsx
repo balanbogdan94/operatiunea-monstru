704ad6dc522d3bc47bf6ccd77a3a2159c3149848
balanbogdan94-pc <balan.bogdan94@gmail.com>
2024-12-22 22:50:30 +0200

    Added logo in jsx component

--- src/components/sections/LandingSection.tsx
+++ src/components/sections/LandingSection.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import BrandLogo from '../common/Logo';
 
 const LandingSection = () => {
-	return <div></div>;
+	return (
+		<div>
+			<BrandLogo />
+		</div>
+	);
 };
 
 export default LandingSection;

6ff2d4295c07800d9745b66076499de717f6c1bc
balanbogdan94-pc <balan.bogdan94@gmail.com>
2024-12-23 00:54:39 +0200

    Added Landing section

--- src/components/sections/LandingSection.tsx
+++ src/components/sections/LandingSection.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import BrandLogo from '../common/Logo';
+import styles from './LandingSection.module.css';
+import SocialMediaIcons from '../common/SocialMedia';
 
 const LandingSection = () => {
 	return (
-		<div>
-			<BrandLogo />
+		<div className={styles.root}>
+			<div style={{ width: '40%', overflow: 'hidden' }}>
+				<BrandLogo
+					preserveAspectRatio={'true'}
+					height={'auto'}
+					width={'100%'}
+					style={{ width: '100%', height: 'auto' }}
+				/>
+			</div>
+			<div>
+				<h1>Operatiunea Monstrul</h1>
+				<h2>Magazinul tau de pescuit si outdoor din Brasov</h2>
+				<SocialMediaIcons />
+			</div>
 		</div>
 	);
 };

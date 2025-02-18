767e65426e613f69f9bd72a893f247cce4718bfb
balanbogdan94-pc <balan.bogdan94@gmail.com>
2025-02-18 23:41:11 +0200

    Moved background change to Landing Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,23 +4,11 @@ import styles from './page.module.css';
 import AdvantagesSection from '@/components/sections/AdvantagesSection';
 import React from 'react';
 import ContactSection from '@/components/sections/ContactSection';
-import { motion, useScroll, useTransform } from 'framer-motion';
+import { motion } from 'framer-motion';
 
 export default function Home() {
-	const { scrollYProgress } = useScroll();
-	const background = useTransform(
-		scrollYProgress,
-		[0, 0.5],
-		[
-			'linear-gradient(to top, #a1c4fd 0%, #c2e9fb 100%)',
-			'linear-gradient(to top, #feada6 0%, #f5efef 100%)',
-		],
-	);
 	return (
-		<motion.div
-			className={styles.page}
-			style={{ background: background }}
-			transition={{ duration: 0.5, type: 'spring' }}>
+		<motion.div className={styles.page}>
 			<main className={styles.main}>
 				<LandingSection />
 
--- src/components/sections/LandingSection.tsx
+++ src/components/sections/LandingSection.tsx
@@ -6,14 +6,26 @@ import SocialMediaIcons from '../common/SocialMedia';
 import { motion, useScroll, useTransform } from 'framer-motion';
 
 const LandingSection = () => {
-	const { scrollY } = useScroll();
+	const { scrollY, scrollYProgress } = useScroll();
+
+	const background = useTransform(
+		scrollYProgress,
+		[0, 0.5],
+		[
+			'linear-gradient(to top, #a1c4fd 0%, #c2e9fb 100%)',
+			'linear-gradient(to top,rgb(61, 103, 177) 0%,rgb(39, 117, 153) 100%)',
+		],
+	);
 
 	const landingTextOpacity = useTransform(scrollY, [0, 200], [1, 0]);
 	const logoOpacity = useTransform(scrollY, [400, 550], [1, 0]);
 	const logoScale = useTransform(scrollY, [400, 550], [1, 0.5]);
 
 	return (
-		<section className={styles.root}>
+		<motion.section
+			className={styles.root}
+			style={{ background }}
+			transition={{ duration: 0.5, type: 'spring' }}>
 			<motion.div
 				className={styles.logo}
 				initial={{ width: '30%', rotate: 0 }}
@@ -42,7 +54,7 @@ const LandingSection = () => {
 					<span className={styles.highlightText}>legendară!</span>
 				</h2>
 			</motion.div>
-		</section>
+		</motion.section>
 	);
 };
 

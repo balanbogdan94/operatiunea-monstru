9af7fc03b13f9921648a14fe52c18dd1080d1f9a
balanbogdan94-pc <balan.bogdan94@gmail.com>
2025-02-20 00:18:43 +0200

    Added SVG for section transition

--- src/components/sections/LandingSection.tsx
+++ src/components/sections/LandingSection.tsx
@@ -54,6 +54,19 @@ const LandingSection = () => {
 					<span className={styles.highlightText}>legendară!</span>
 				</h2>
 			</motion.div>
+			<div className={styles.bottomCircle}>
+				<svg
+					height='100%'
+					width='100%'
+					viewBox='0 0 868 134'
+					preserveAspectRatio='xMidYMid meet'
+					xmlns='http://www.w3.org/2000/svg'>
+					<path
+						d='M868 0.62854C773.797 71.9901 613.015 119 430.5 119C252.43 119 95.0469 74.2518 0 5.79834V134H868V0.62854Z'
+						fill='#FFFFFF'
+					/>
+				</svg>
+			</div>
 		</motion.section>
 	);
 };

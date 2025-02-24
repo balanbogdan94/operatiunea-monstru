00dab2da691985f3e80808521e67225dc26d9142
balanbogdan94-pc <balan.bogdan94@gmail.com>
2025-02-24 23:25:58 +0200

    advantages sections animations

--- src/components/sections/AdvantagesSection.tsx
+++ src/components/sections/AdvantagesSection.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import styles from './AdvantagesSection.module.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHiking, faUserFriends } from '@fortawesome/free-solid-svg-icons';
 import { motion } from 'framer-motion';
 import Image from 'next/image';
 
@@ -12,64 +10,105 @@ const AdvantagesSection = () => {
 	};
 
 	const h2Variants = {
-		initial: { scale: 1 },
-		inView: { scale: 0.8 },
+		initial: { color: '#2a3541', scale: 0.7 },
+		inView: { color: '#ffffff', scale: 1 },
 	};
 
-	const getUlVariants = (index: number) => ({
-		initial: {
-			x: -300,
-			rotate: !index ? 0 : index % 2 === 0 ? 10 : -10,
-		},
-		inView: { y: 0, rotate: 0, x: 0 },
-	});
+	const liVariants = {
+		initial: { opacity: 0, y: 50 },
+		inView: { opacity: 1, y: 0 },
+	};
 	return (
-		<motion.div
-			animate={variants}
-			initial='initial'
-			whileInView='inView'
-			className={styles.root}
-			variants={variants}
-			viewport={{ amount: 0.7, once: true }}>
-			<motion.h2 variants={h2Variants} transition={{ duration: 0.2 }}>
-				Descopera ce ti-am pregatit!
-			</motion.h2>
-			<motion.ul variants={variants}>
-				<motion.li variants={getUlVariants(0)} transition={{ duration: 0.5 }}>
-					<div
-						style={{
-							position: 'relative',
-							width: '100%',
-							aspectRatio: 16 / 12,
-						}}>
-						<Image
-							style={{ borderRadius: 10 }}
-							objectFit='cover'
-							src={'/top-equpment.jpg'}
-							alt={''}
-							fill
-						/>
-					</div>
-					<h3>Echipamente de top</h3>
-					<span style={{ textAlign: 'end' }}>
-						Undițe, momeli, accesorii şi multe altele,
-						<b style={{ fontSize: 16 }}> toate de calitate!</b>
-					</span>
-				</motion.li>
-				<motion.li variants={getUlVariants(1)} transition={{ duration: 0.2 }}>
-					<FontAwesomeIcon icon={faHiking} />
-					<h3>Produse pentru aventuri în natură</h3>
-					<span>De la îmbrăcăminte outdoor la gadgeturi utile.</span>
-				</motion.li>
-				<motion.li variants={getUlVariants(2)} transition={{ duration: 0.4 }}>
-					<FontAwesomeIcon icon={faUserFriends} />
-					<h3>Consiliere dedicata</h3>
-					<span>
-						Echipa noastră pasionată te va ajuta să alegi exact ce îți trebuie!
-					</span>
-				</motion.li>
-			</motion.ul>
-		</motion.div>
+		<>
+			<motion.div
+				animate={variants}
+				initial='initial'
+				whileInView='inView'
+				className={styles.root}
+				variants={variants}
+				viewport={{ amount: 0.3 }}>
+				<motion.h2 variants={h2Variants} transition={{ duration: 0.3 }}>
+					Descopera ce ti-am pregatit!
+				</motion.h2>
+				<motion.ul variants={variants}>
+					<motion.li variants={liVariants} transition={{ duration: 0.3 }}>
+						<motion.div
+							style={{
+								position: 'relative',
+								width: '100%',
+								aspectRatio: 16 / 12,
+							}}>
+							<Image
+								style={{ borderRadius: 10 }}
+								objectFit='cover'
+								src={'/top-equpment.jpg'}
+								alt={''}
+								fill
+							/>
+						</motion.div>
+						<h3>Echipamente de top</h3>
+						<span style={{ textAlign: 'end' }}>
+							Undițe, momeli, accesorii şi multe altele,
+							<b style={{ fontSize: 16 }}> toate de calitate!</b>
+						</span>
+					</motion.li>
+					<motion.li variants={liVariants} transition={{ duration: 0.3 }}>
+						<div
+							style={{
+								position: 'relative',
+								width: '100%',
+								aspectRatio: 16 / 12,
+							}}>
+							<Image
+								style={{ borderRadius: 10 }}
+								objectFit='cover'
+								src={'/top-equpment.jpg'}
+								alt={''}
+								fill
+							/>
+						</div>
+						<h3>Produse pentru aventuri în natură</h3>
+						<span>
+							De la <b style={{ fontSize: 16 }}>îmbrăcăminte outdoor</b> la{' '}
+							<b style={{ fontSize: 16 }}>gadgeturi utile.</b>
+						</span>
+					</motion.li>
+					<motion.li>
+						<div
+							style={{
+								position: 'relative',
+								width: '100%',
+								aspectRatio: 16 / 12,
+							}}>
+							<Image
+								style={{ borderRadius: 10 }}
+								objectFit='cover'
+								src={'/top-equpment.jpg'}
+								alt={''}
+								fill
+							/>
+						</div>
+						<h3>Consiliere dedicata</h3>
+						<span style={{ textAlign: 'start' }}>
+							Echipa noastră pasionată te va ajuta să alegi{' '}
+							<b style={{ fontSize: 16 }}>exact ce îți trebuie!</b>
+						</span>
+					</motion.li>
+				</motion.ul>
+			</motion.div>
+			<svg
+				height='100%'
+				width='100%'
+				style={{ marginTop: '-5px' }}
+				viewBox='0 0 868 134'
+				fill='none'
+				xmlns='http://www.w3.org/2000/svg'>
+				<path
+					d='M0 134C94.2031 62.6385 254.985 15.6285 437.5 15.6285C615.57 15.6285 772.953 60.3768 868 128.83V0.62854L0 0.62854V134Z'
+					fill='#395e7c'
+				/>
+			</svg>
+		</>
 	);
 };
 

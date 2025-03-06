35cb7cf487cf00b796429a37313d10e957441cc4
balanbogdan94-pc <balan.bogdan94@gmail.com>
2025-03-07 00:11:51 +0200

    Replaced Iframe with google maps component

--- src/components/sections/ContactSection.tsx
+++ src/components/sections/ContactSection.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import styles from './ContactSection.module.css';
 import { motion } from 'framer-motion';
+import { APIProvider, AdvancedMarker, Map } from '@vis.gl/react-google-maps';
+
+const position: google.maps.LatLngLiteral = { lat: 45.667519, lng: 25.608705 };
+const apiKey = process.env.GOOGLE_MAPS_API_KEY ?? '';
 
 const ContactSection = () => {
 	return (
@@ -13,14 +17,14 @@ const ContactSection = () => {
 			</motion.svg>
 			<h2>Unde ne gasesti</h2>
 			<div className={styles.mapContainer}>
-				<iframe
-					src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2788.1688131407714!2d25.606211376285085!3d45.66751127107806!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40b35bed41decc07%3A0xc9150009ba9a1a1f!2sBloc%204%2C%20Aleea%20Constructorilor%204%2C%20Bra%C8%99ov%20507190!5e0!3m2!1sen!2sro!4v1734977702471!5m2!1sen!2sro'
-					width='600'
-					height='450'
-					allowFullScreen
-					loading='lazy'
-					referrerPolicy='no-referrer-when-downgrade'
-				/>
+				<APIProvider apiKey={apiKey}>
+					<Map
+						defaultCenter={position}
+						defaultZoom={10}
+						mapId='om-947d8959-0ec1-4c04-879f-0885ab92343d'>
+						<AdvancedMarker position={position} />
+					</Map>
+				</APIProvider>
 			</div>
 			<h2>Contactează-ne</h2>
 			<form className={styles.contactForm}>

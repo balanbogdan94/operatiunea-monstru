da7c19fd80be726a4e3b6011813106d923e6aeaa
balanbogdan94-pc <balan.bogdan94@gmail.com>
2025-03-26 22:57:23 +0200

    Changes for Landing section desktop

--- src/components/sections/LandingSection.tsx
+++ src/components/sections/LandingSection.tsx
@@ -4,9 +4,11 @@ import BrandLogo from '../common/Logo';
 import styles from './LandingSection.module.css';
 import SocialMediaIcons from '../common/SocialMedia';
 import { motion, useScroll, useTransform } from 'framer-motion';
+import { useMediaQuery } from 'react-responsive';
 
 const LandingSection = () => {
 	const { scrollY, scrollYProgress } = useScroll();
+	const isMobile = useMediaQuery({ maxWidth: 768 });
 
 	const background = useTransform(
 		scrollYProgress,
@@ -30,7 +32,9 @@ const LandingSection = () => {
 				className={styles.logo}
 				initial={{ width: '30%', rotate: 0 }}
 				animate={{ width: '100%', rotate: 360 }}
-				style={{ opacity: logoOpacity, scale: logoScale }}
+				style={
+					isMobile ? { opacity: logoOpacity, scale: logoScale } : undefined
+				}
 				transition={{ duration: 1, type: 'spring', stiffness: 100 }}>
 				<BrandLogo />
 			</motion.div>

d75d2c9a43ab7474f5d525aa135cbee4de6b33f6
balanbogdan94-pc <balan.bogdan94@gmail.com>
2025-03-27 23:53:00 +0200

    Fix minor issues

--- src/components/sections/LandingSection.tsx
+++ src/components/sections/LandingSection.tsx
@@ -60,10 +60,8 @@ const LandingSection = () => {
 			</motion.div>
 			<div className={styles.bottomCircle}>
 				<svg
-					height='100%'
-					width='100%'
 					viewBox='0 0 868 134'
-					preserveAspectRatio='xMidYMid meet'
+					preserveAspectRatio='none'
 					xmlns='http://www.w3.org/2000/svg'>
 					<path
 						d='M868 0.62854C773.797 71.9901 613.015 119 430.5 119C252.43 119 95.0469 74.2518 0 5.79834V134H868V0.62854Z'

8c3b95e9eebe381a150f792cc2a704a2ff24fbde
balanbogdan94-pc <balan.bogdan94@gmail.com>
2025-04-10 00:33:29 +0300

    Add UnderWaterView component with animated bubbles effect

--- src/components/sections/LandingSection.tsx
+++ src/components/sections/LandingSection.tsx
@@ -5,6 +5,7 @@ import styles from './LandingSection.module.css';
 import SocialMediaIcons from '../common/SocialMedia';
 import { motion, useScroll, useTransform } from 'framer-motion';
 import { useMediaQuery } from 'react-responsive';
+import UnderWaterView from '../common/UnderWaterView';
 
 const LandingSection = () => {
 	const { scrollY, scrollYProgress } = useScroll();
@@ -24,53 +25,55 @@ const LandingSection = () => {
 	const logoScale = useTransform(scrollY, [400, 550], [1, 0.5]);
 
 	return (
-		<motion.section
-			className={styles.root}
-			style={{ background }}
-			transition={{ duration: 0.5, type: 'spring' }}>
-			<motion.div
-				className={styles.logo}
-				initial={{ width: '30%', rotate: 0 }}
-				animate={{ width: '100%', rotate: 360 }}
-				style={
-					isMobile ? { opacity: logoOpacity, scale: logoScale } : undefined
-				}
-				transition={{ duration: 1, type: 'spring', stiffness: 100 }}>
-				<BrandLogo />
-			</motion.div>
-			<motion.div
-				className={styles.landingText}
-				initial={{ opacity: 0 }}
-				animate={{ opacity: 1 }}
-				style={{ opacity: landingTextOpacity }}
-				transition={{ duration: 0.1 }}>
-				<h1 id='scrollText'>Operatiunea Monstrul</h1>
-				<h2>
-					Magazinul tau de pescuit si outdoor din{' '}
-					<span className={styles.highlightText}>Brasov!</span>
-				</h2>
-				<SocialMediaIcons />
-			</motion.div>
-			<motion.div className={styles.callToAction}>
-				<h2>
-					Fă din fiecare ieșire pe{' '}
-					<span className={styles.highlightText}>apă</span> o aventură{' '}
-					<span className={styles.highlightText}>legendară!</span>
-				</h2>
-			</motion.div>
-			<div className={styles.bottomCircle}>
-				<svg
-					viewBox='0 0 868 134'
-					preserveAspectRatio='none'
-					width='100%'
-					xmlns='http://www.w3.org/2000/svg'>
-					<path
-						d='M868 0.62854C773.797 71.9901 613.015 119 430.5 119C252.43 119 95.0469 74.2518 0 5.79834V134H868V0.62854Z'
-						fill='#2a3541'
-					/>
-				</svg>
-			</div>
-		</motion.section>
+		<UnderWaterView bubbleCount={8} minSize={20} maxSize={70}>
+			<motion.section
+				className={styles.root}
+				style={{ background }}
+				transition={{ duration: 0.5, type: 'spring' }}>
+				<motion.div
+					className={styles.logo}
+					initial={{ width: '30%', rotate: 0 }}
+					animate={{ width: '100%', rotate: 360 }}
+					style={
+						isMobile ? { opacity: logoOpacity, scale: logoScale } : undefined
+					}
+					transition={{ duration: 1, type: 'spring', stiffness: 100 }}>
+					<BrandLogo />
+				</motion.div>
+				<motion.div
+					className={styles.landingText}
+					initial={{ opacity: 0 }}
+					animate={{ opacity: 1 }}
+					style={{ opacity: landingTextOpacity }}
+					transition={{ duration: 0.1 }}>
+					<h1 id='scrollText'>Operatiunea Monstrul</h1>
+					<h2>
+						Magazinul tau de pescuit si outdoor din{' '}
+						<span className={styles.highlightText}>Brasov!</span>
+					</h2>
+					<SocialMediaIcons />
+				</motion.div>
+				<motion.div className={styles.callToAction}>
+					<h2>
+						Fă din fiecare ieșire pe{' '}
+						<span className={styles.highlightText}>apă</span> o aventură{' '}
+						<span className={styles.highlightText}>legendară!</span>
+					</h2>
+				</motion.div>
+				<div className={styles.bottomCircle}>
+					<svg
+						viewBox='0 0 868 134'
+						preserveAspectRatio='none'
+						width='100%'
+						xmlns='http://www.w3.org/2000/svg'>
+						<path
+							d='M868 0.62854C773.797 71.9901 613.015 119 430.5 119C252.43 119 95.0469 74.2518 0 5.79834V134H868V0.62854Z'
+							fill='#2a3541'
+						/>
+					</svg>
+				</div>
+			</motion.section>
+		</UnderWaterView>
 	);
 };
 

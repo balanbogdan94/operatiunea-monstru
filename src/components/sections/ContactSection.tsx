93757bc86f95664d7fec29cef77d2012ec4ac6a0
balanbogdan94-pc <balan.bogdan94@gmail.com>
2025-05-15 00:28:30 +0300

    Refactor ContactSection styles and structure for improved layout; enhance map iframe styling and dimensions

--- src/components/sections/ContactSection.tsx
+++ src/components/sections/ContactSection.tsx
@@ -25,9 +25,7 @@ const ContactSection = () => {
 				<iframe
 					title='Location Map'
 					src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2787.113568226502!2d25.59422!3d45.64855!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40b35b7cce0f0001%3A0x9c1e87efe7a2204!2sAleea%20Constructorilor%204%2C%20Bra%C8%99ov%20500388!5e0!3m2!1sen!2sro!4v1715631193450!5m2!1sen!2sro'
-					width='100%'
-					height='100%'
-					style={{ border: 0 }}
+					className={styles.mapIframe}
 					allowFullScreen
 					loading='lazy'
 					referrerPolicy='no-referrer-when-downgrade'></iframe>
